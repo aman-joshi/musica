6aabe2cae1f0cadad8233079bcff1b0ca7185f83
Aman Joshi <aman.joshi@crownstack.com>
2021-08-15 08:32:25 +0530

    Initial Commit

---
+++ Musica/MusicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicaApp.swift
+//  Musica
+//
+//  Created by Aman Joshi on 15/08/21.
+//
+
+import SwiftUI
+
+@main
+struct MusicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
